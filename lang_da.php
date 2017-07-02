53a35960e8ecb8c041155b02f1264d1a085fa227
henrik8000 <henrik8000@users.noreply.github.com>
2017-07-03 00:22:59 +0700

    lang_da.php
    // language: Danish

---
+++ lang_da.php
@@ -0,0 +1,60 @@
+<?php
+// language: Danish	
+// made by: henrik8000	
+// updated on: 03.July.2017
+
+// MENU
+define('_SEARCH', 'søg');
+define('_PURCHASE', 'køb');
+define('_SUPPORT', 'support');
+define('_ACCOUNT', 'konto');
+define('_ACCOUNT_SETTINGS', 'Kontoindstillinger');
+define('_ACCOUNT_CHANGE', 'indstillinger for webstedet'); //client: website, eg font type, theme
+define('_AFFILIATE', 'affiliate');
+define('_AFFILIATE_NEW', 'se indtjening');
+define('_AFFILIATE_KEYS', 'ændre nøgler');
+
+// ACCOUNT
+define('_SIGN_OUT', 'Log ud');
+define('_WELCOME', 'velkommen,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternative metoder at kontakte os på via twitter eller vores e-mail adresse publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'vores mest aktive support tider er');
+define('_MONDAY_FRIDAY', 'Mandag til Fredag.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'effektivitetsoversigt');
+define('_AVG_QUERY', 'gennemsnitlige forespørgsler');
+define('_AVG_RESULTS', 'gennemsnitlige resultater');
+define('_ACC_NUMBER', 'konto nummer');
+define('_API_LOAD', 'api belastning');
+define('_SHORT_TERM', 'kortsigtede mål');
+define('_LONG_TERM', 'langsigtede mål');
+define('_MOST_ACTIVE', 'Vi stræber konstant efter at forbedre ydeevnen, på grund af det gør vi vores statistik offentlige for alle til at se. Disse nulstilles i begyndelsen af hver måned, og når vi laver store api opdateringer.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'skift brugernavn');
+define('_CHANGE_USERNAME_DISPLAY', 'ændre viste navne');
+define('_CHANGE_EMAIL', 'skift email');
+define('_CHANGE_PASSWORD', 'skift adgangskode');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'køb');
+define('_X_DAY_ACCESS', 'dages adgang'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'ubegrænset adgang til søge funktioner');
+define('_BASIC_WILDCARD', 'grundlæggende wildcard');
+define('_CUSTOM_WILDCARD', 'brugerdefinerede wildcard');
+define('_PRIORITIZED_SUPPORT', 'prioriteret support');
+define('_PRIVATE_ENDPOINT', 'privat API slutpunkt');
+define('_ALL_FEATURES', 'alle funktioner af Demo og Premium');
+define('_DAY_EMAIL_SUPPORT', '24/7 email support');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'affiliate system');
+define('_AFFILIATE_MADE', 'penge tjent');
+define('_AFFILIATE_KEY', 'affiliate nøgle');
+define('_AFFILIATE_CREATE', 'opret ny nøgle');
+define('_AFFILIATE_WITHDRAW', 'udbetal');
+
+?>
