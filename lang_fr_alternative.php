19e465306472add1ccc3e9c952636d6f36b0ad12
publicdbhost <jgt@cock.li>
2017-07-09 13:19:42 +0200

    Create lang_fr_alternative.php

---
+++ lang_fr_alternative.php
@@ -0,0 +1,52 @@
+<?php
+// language: French
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+// MENU
+define('_SEARCH', 'Rechercher');
+define('_PURCHASE', 'Acheter');
+define('_SUPPORT', 'Support');
+define('_ACCOUNT', 'Compte');
+define('_ACCOUNT_SETTINGS', 'Paramètres du compte');
+define('_ACCOUNT_CHANGE', 'Paramètres d\'affichage'); //client: website, eg font type, theme
+define('_AFFILIATE', 'Parrainage');
+define('_AFFILIATE_NEW', 'Voir vos gains');
+define('_AFFILIATE_KEYS', 'Changer vos clés');
+// ACCOUNT
+define('_SIGN_OUT', 'Déconnecter');
+define('_WELCOME', 'Bienvenue,'); //eg Welcome, Username.
+// SUPPORT
+define('_SUPPORT', 'Les autres méthodes de contct sont notre twitter et notre email publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'Notre support est le plus actif aux heures suivantes : ');
+define('_MONDAY_FRIDAY', 'de Lundi à Vendredi');
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'Vue d\'ensemble des performances');
+define('_AVG_QUERY', 'Temps de requete moyen');
+define('_AVG_RESULTS', 'Nombre de resultat moyen');
+define('_ACC_NUMBER', 'Nombre d\'utilisateur');
+define('_API_LOAD', 'Charge de l\'API');
+define('_SHORT_TERM', 'Buts à court terme');
+define('_LONG_TERM', 'Buts à long terme');
+define('_MOST_ACTIVE', 'Nous essayons continuellement d\'améliorer les performances, et c\'est pour cette raison que nous rendons toutes nos statistiques accessibles à tous. Nous réinitilisons les statistiques au début de chaque mois ou quand une mise à jour de l\' API sort.');
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'Changer de nom d\'utilisateur');
+define('_CHANGE_USERNAME_DISPLAY', 'Changer le nom affiché');
+define('_CHANGE_EMAIL', 'Changer d\'adresse email');
+define('_CHANGE_PASSWORD', 'Changer de mot de passe');
+// PURCHASE PAGE
+define('_PURCHASE', 'Acheter');
+define('_X_DAY_ACCESS', 'jours d\'accès'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'Accès illimité aux fonctionnalités de recherche');
+define('_BASIC_WILDCARD', 'Wildcard basique');
+define('_CUSTOM_WILDCARD', 'Wildcard personnalisé');
+define('_PRIORITIZED_SUPPORT', 'Accès au support prioritaire');
+define('_PRIVATE_ENDPOINT', 'Point de terminaison privé de l\'API');
+define('_ALL_FEATURES', 'Toutes les fonctionnalités du Demo et du Premium');
+define('_DAY_EMAIL_SUPPORT', 'Support par mail 24h/24 7j/7');
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'système de parrainage');
+define('_AFFILIATE_MADE', 'argent généré');
+define('_AFFILIATE_KEY', 'clé de parrain');
+define('_AFFILIATE_CREATE', 'créer une nouvelle clé');
+define('_AFFILIATE_WITHDRAW', 'retirer');
+?>
