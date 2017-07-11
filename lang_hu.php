9565cd312aeb59b362ff7b2e73a95fb18470efb9
publicdbhost <jgt@cock.li>
2017-07-12 00:08:18 +0200

    Create lang_hu.php

---
+++ lang_hu.php
@@ -0,0 +1,60 @@
+<?php
+// language: Hungarian
+// made by: Snusbase Translate Team
+// updated on: 22.June.2017
+
+// MENU
+define('_SEARCH', 'keresés');
+define('_PURCHASE', 'vásárlás');
+define('_SUPPORT', 'támogatás');
+define('_ACCOUNT', 'fiók');
+define('_ACCOUNT_SETTINGS', 'fiókbeállítások');
+define('_ACCOUNT_CHANGE', 'weboldal beállítások'); //client: website, eg font type, theme
+define('_AFFILIATE', 'megosztás');
+define('_AFFILIATE_NEW', 'jövedelem mutatása');
+define('_AFFILIATE_KEYS', 'kulcsok cserélése');
+
+// ACCOUNT
+define('_SIGN_OUT', 'kijelentkezés');
+define('_WELCOME', 'Üdvözlet,'); //eg Welcome, Username.
+
+// SUPPORT
+define('_SUPPORT', 'alternatív kapcsolatfelvételi módok a Twitterünk és az email-címünk, ami publicdbhost@gmail.com');
+define('_MOST_ACTIVE', 'legaktívabb támogatási óráink');
+define('_MONDAY_FRIDAY', 'hétfőtől péntekig.');
+
+// MEMBERS PAGE
+define('_P_OVERVIEW', 'teljesítmény áttekintés');
+define('_AVG_QUERY', 'átlagos találatok');
+define('_AVG_RESULTS', 'átlagos eredmények');
+define('_ACC_NUMBER', 'fiókszám');
+define('_API_LOAD', 'API betöltés');
+define('_SHORT_TERM', 'rövid távú célok');
+define('_LONG_TERM', 'hosszú távú célok');
+define('_MOST_ACTIVE', 'Folyamatosan törekszünk a teljesítmény javítására, ezért mindenki számára nyilvánossá tesszük statisztikáinkat. Ezek a visszaállítások minden hónap elején történnek és akkor, amikor nagy API frissítéseket végzünk.');
+
+// ACCOUNT PAGE
+define('_CHANGE_USERNAME', 'felhasználónév változtatás');
+define('_CHANGE_USERNAME_DISPLAY', 'megjelenítendő név változtatás');
+define('_CHANGE_EMAIL', 'email-változtatás');
+define('_CHANGE_PASSWORD', 'jelszóváltoztatás');
+
+// PURCHASE PAGE
+define('_PURCHASE', 'vásárlás');
+define('_X_DAY_ACCESS', 'napos hozzáférés'); //EG "7 DAY ACCESS"
+define('_SEARCH_FEATURES', 'korlátlan hozzáférés a keresési funkcióhoz');
+define('_BASIC_WILDCARD', 'alap helyettesítő karakter');
+define('_CUSTOM_WILDCARD', 'egyedi helyettesítő karakter');
+define('_PRIORITIZED_SUPPORT', 'kiemelt támogatás');
+define('_PRIVATE_ENDPOINT', 'privát API végpont');
+define('_ALL_FEATURES', 'a demó és a prémium összes funkciója');
+define('_DAY_EMAIL_SUPPORT', '24/7 e-mail-támogatás');
+
+// AFFILIATE PAGE
+define('_AFFILIATE_WELCOME', 'megosztórendszer');
+define('_AFFILIATE_MADE', 'pénzkereset');
+define('_AFFILIATE_KEY', 'megosztókulcs');
+define('_AFFILIATE_CREATE', 'új kulcs létrehozása');
+define('_AFFILIATE_WITHDRAW', 'felvétel');
+
+?>
